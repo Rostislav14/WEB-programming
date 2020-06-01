f3a51f702f1b3861be5c3ea97c4ff65a1ba550dc
Rostislav14 <54891530+Rostislav14@users.noreply.github.com>
2020-06-01 12:10:10 +0300

    Add files via upload

---
+++ pars.js
@@ -0,0 +1,12 @@
+const  request  =  require ( "request" ) ;
+var  url  =  'https://www.shazam.com/shazam/v2/ru/UA/web/-/tracks/world-chart-world?pageSize=300&startFrom=0'
+request ( url ,  ( err ,  response ,  json )  =>  {
+    if  ( ! err )  {
+        var  obj  =  JSON . синтаксический анализ ( JSON ) ;
+        для  ( var  i  =  0 ;  i  <  300 ;  i ++ )  {
+            консоль . log ( ( obj [ 'chart' ] [ i ] [ 'share' ] [ 'subject' ] ) + "" + ( obj [ 'chart' ] [ i ] [ 'share' ] [ 'href' ] ) )
+        }
+    }
+} )
+  }
+} )
